c3f0b63602dc87c0ab769f2bc8b6e58d3415d118
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-01-17 21:13:13 +0100

    add remote fetch suport based on Fetch API

--- full/test/utils/fetch.js
+++ full/test/utils/fetch.js
@@ -16,11 +16,118 @@ export function getLocalClient() {
 }
 
 /**
- * Get a remote client
- * @returns {Function} - Fetch client
+ * Get a remote client: Use the following code to test a remote API
  */
 export function getRemoteClient() {
-    // Use the following code to test a remote API
     // it could be Axios, Fetch, or any other client
-    return fetch;
+    return {
+        /**
+         * POST method
+         * @param {string} url 
+         * @param {object} [data] 
+         * @param {object} [headers] 
+         * @returns {Promise<{status: Number, body: Object, error?: Error}>}
+         */
+        post: async function (url, data = {}, headers = {}) {
+            try {
+                const response = await fetch(url, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json', ...headers },
+                    body: JSON.stringify(data),
+                });
+                const body = await response.json();
+                if (!response.ok) {
+                    return {
+                        body,
+                        error: new Error(body?.message || (typeof body === "string" && body) || `Create action failed`),
+                        status: response.status
+                    };
+                }
+                return { status: response.status, body };
+            }
+            catch (error) {
+                return { error, status: 500 };
+            }
+        },
+        /**
+         * GET method
+         * @param {string} url 
+         * @param {object} headers 
+         * @returns {Promise<{status: Number, body: Object, error?: Error}>}
+         */
+        get: async function (url, headers = {}) {
+            try {
+                const response = await fetch(url, {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json', ...headers },
+                });
+                const body = await response.json();
+                if (!response.ok) {
+                    return {
+                        body,
+                        error: new Error(body?.message || (typeof body === "string" && body) || `Select action failed`),
+                        status: response.status
+                    };
+                }
+                return { status: response.status, body };
+            }
+            catch (error) {
+                return { error, status: 500 };
+            }
+        },
+        /**
+         * PUT method
+         * @param {string} url 
+         * @param {object} [data] 
+         * @param {object} [headers] 
+         * @returns {Promise<{status: Number, body: Object, error?: Error}>}
+         */
+        put: async function (url, data = {}, headers = {}) {
+            try {
+                const response = await fetch(url, {
+                    method: 'PUT',
+                    headers: { 'Content-Type': 'application/json', ...headers },
+                    body: JSON.stringify(data),
+                });
+                const body = await response.json();
+                if (!response.ok) {
+                    return {
+                        body,
+                        error: new Error(body?.message || (typeof body === "string" && body) || `Update action failed`),
+                        status: response.status
+                    };
+                }
+                return { status: response.status, body };
+            }
+            catch (error) {
+                return { error, status: 500 };
+            }
+        },
+        /**
+         * DELETE method
+         * @param {string} url 
+         * @param {object} [headers] 
+         * @returns {Promise<{status: Number, body: Object, error?: Error}>}
+         */
+        delete: async function (url, headers = {}) {
+            try {
+                const response = await fetch(url, {
+                    method: 'DELETE',
+                    headers: { 'Content-Type': 'application/json', ...headers },
+                });
+                const body = await response.json();
+                if (!response.ok) {
+                    return {
+                        body,
+                        error: new Error(body?.message || (typeof body === "string" && body) || `Delete action failed`),
+                        status: response.status
+                    };
+                }
+                return { status: response.status, body };
+            }
+            catch (error) {
+                return { error, status: 500 };
+            }
+        }
+    };
 }
