53398d5d0a30f9ea4e1667cac6af448518fe445f
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-01-17 18:21:01 +0100

    add test with object pages

--- full/test/e2e/playwright/from.spec.js
+++ full/test/e2e/playwright/from.spec.js
@@ -1,11 +1,14 @@
 import { expect, test } from '@playwright/test';
-import { getUser } from '../../utils/fake.js';
+import * as userFakeService from '../../utils/fake.js';
+import { UserFormPage } from './pages/user.form.page.js';
+import { UserHomePage } from './pages/user.home.page.js';
 
 test.describe('Testing the User Module', () => {
     test('should create an account with username and password', async ({
         page,
     }) => {
-        const user = getUser();
+        // Get a random user
+        const user = userFakeService.getUser();
         // Step 1: Navigate to the form page
         await page.goto('http://localhost:3000/user/form');  // Replace with your actual URL
 
@@ -28,5 +31,32 @@ test.describe('Testing the User Module', () => {
         // Optionally, verify that the user data appears in the list
         const userName = await page.locator(`td:has-text("${user.firstName} ${user.lastName}")`);
         await expect(userName).toBeVisible();  // Ensure the new user appears in the user list
+    });
+
+
+    test('should create an account with username and password using Page Object', async ({
+        page,
+    }) => {
+        // Get a random user
+        const user = userFakeService.getUser();
+        
+        // Dedeclare the Page Object instances
+        const userFormPage = new UserFormPage(page);
+        const userHomePage = new UserHomePage(page);
+
+        // Step 1: Navigate to the form page
+        await userFormPage.goto(); 
+
+        // Step 2: Fill in the form fields
+        await userFormPage.fillForm(user);
+
+        // Step 3: Submit the form
+        await userFormPage.btnRegisterClick();
+
+        // Step 4: Wait for the response (e.g., a confirmation message or a user list)
+        await userHomePage.checkUrl()  // Replace with your actual redirect URL
+
+        // Optionally, verify that the user data appears in the list
+        await userHomePage.checkRow(user);
     });
 });
